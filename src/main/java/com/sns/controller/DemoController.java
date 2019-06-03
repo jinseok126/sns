f75eb9185beae06e186d5deb330ef002372d2352
jinseok126 <jinseok126@naver.com>
2019-06-03 21:40:31 +0900

    spring boot mysql jpa connect

---
+++ src/main/java/com/sns/controller/DemoController.java
@@ -0,0 +1,22 @@
+package com.sns.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import lombok.extern.slf4j.Slf4j;
+
+
+@Controller
+@Slf4j
+public class DemoController {
+	
+	@GetMapping("/")
+	public String home(Model model) {
+		
+		log.info("home");
+		model.addAttribute("java", "1.8");
+		return "home";
+	}
+
+}
---
+++ src/main/webapp/WEB-INF/views/home.jsp
@@ -0,0 +1,13 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8" pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html lang="ko">
+<script src="/resources/css/bootstrap.min.css"></script>
+<head>
+<meta charset="UTF-8">
+<title>demo</title>
+</head>
+<body>
+demo jsp<br>
+<p class="text-muted">java : ${java}</p>
+</body>
+</html>
